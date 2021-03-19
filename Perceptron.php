2ab8695ee49e70cc266bda4ef42ba022700f7335
Sergey <circumflex@gmailc.com>
2021-03-19 10:52:19 +0200

    Tabs

--- Perceptron.php
+++ Perceptron.php
@@ -27,20 +27,21 @@ class Perceptron
 
     public function __construct($learningRate = 0.98, $errorTrashhold = 0.0001)
     {
-        $this->neurones  = [];
+        $this->neurones = [];
         $this->layers = [];
         $this->totalError = 0;
         $this->learningRate = $learningRate;
         $this->errorTrashold = $errorTrashhold;
     }
 
-    public function addNeuron(Cell $cell, $id, $layer, $bias = 0) {
+    public function addNeuron(Cell $cell, $id, $layer, $bias = 0)
+    {
         $this->neurones[$id] = [
             'id' => $id,
             'cell' => $cell,
             'links' => [],
             'layer' => $layer,
-            'bias' => $bias
+            'bias' => $bias,
         ];
 
         $this->indexLayers();
@@ -96,17 +97,23 @@ class Perceptron
 
     public function getNeuronsByLayer($layerNumber)
     {
-        return array_filter($this->neurones, function ($neuron) use ($layerNumber) {
-             return $neuron['layer'] == $layerNumber;
-        });
+        return array_filter(
+            $this->neurones,
+            function ($neuron) use ($layerNumber) {
+                return $neuron['layer'] == $layerNumber;
+            }
+        );
     }
 
     public function getNeuronLinks($neuron, $linkType)
     {
         $neurones = [];
-        $links = array_filter($neuron['links'], function ($link) use ($linkType) {
-            return $link['type'] == $linkType;
-        });
+        $links = array_filter(
+            $neuron['links'],
+            function ($link) use ($linkType) {
+                return $link['type'] == $linkType;
+            }
+        );
 
         foreach ($links as $link) {
             $neurones[] = [
@@ -128,33 +135,39 @@ class Perceptron
         }
 
         $n1 = $this->neurones[$id1];
-		$n2 = $this->neurones[$id2];
+        $n2 = $this->neurones[$id2];
 
-		$n1['links'][] = ['id' => $id2, 'weight' => $weight, 'type' => 'right'];
-		$n2['links'][] = ['id' => $id1, 'weight' => $weight, 'type' => 'left'];
-		$this->updateNeuron($id1, $n1);
-		$this->updateNeuron($id2, $n2);
-	}
+        $n1['links'][] = ['id' => $id2, 'weight' => $weight, 'type' => 'right'];
+        $n2['links'][] = ['id' => $id1, 'weight' => $weight, 'type' => 'left'];
+        $this->updateNeuron($id1, $n1);
+        $this->updateNeuron($id2, $n2);
+    }
 
     public function unlink($id1, $id2)
     {
         $n1 = $this->neurones[$id1];
         $n2 = $this->neurones[$id2];
-        $n1['links'] = array_filter($n1['links'], function($link) use ($id2) {
-            return $link['id'] != $id2;
-        });
-        $n2['links'] = array_filter($n2['links'], function($link) use ($id1) {
-            return $link['id'] != $id1;
-        });
+        $n1['links'] = array_filter(
+            $n1['links'],
+            function ($link) use ($id2) {
+                return $link['id'] != $id2;
+            }
+        );
+        $n2['links'] = array_filter(
+            $n2['links'],
+            function ($link) use ($id1) {
+                return $link['id'] != $id1;
+            }
+        );
         $this->updateNeuron($id1, $n1);
         $this->updateNeuron($id2, $n2);
     }
 
-    public function linkAll() 
+    public function linkAll()
     {
         foreach ($this->layers as $layer) {
-            
-            $neuronesLeft  = $this->getNeuronsByLayer($layer);
+
+            $neuronesLeft = $this->getNeuronsByLayer($layer);
             $neuronesRight = $this->getNeuronsByLayer($layer + 1);
 
             foreach ($neuronesLeft as $leftNeuron) {
@@ -165,7 +178,7 @@ class Perceptron
         }
     }
 
-    public function createLayers($neuronsCountArray, $linkAutomatically = true) 
+    public function createLayers($neuronsCountArray, $linkAutomatically = true)
     {
         foreach ($neuronsCountArray as $layer => $neuronsNumber) {
             for ($number = 0; $number < $neuronsNumber; $number++) {
@@ -179,12 +192,12 @@ class Perceptron
             }
         }
 
-		if ($linkAutomatically) {
+        if ($linkAutomatically) {
             $this->linkAll();
         }
-	}
+    }
 
-    public function setInputVector($inputsArray) 
+    public function setInputVector($inputsArray)
     {
         $firstLayerNeurones = $this->getNeuronsByLayer($this->layers[0]);
 
@@ -193,9 +206,9 @@ class Perceptron
             $firstLayerNeuron['cell']->setInput($inputsArray[$key++]);
             $this->updateNeuron($firstLayerNeuron['id'], $firstLayerNeuron);
         }
-	}
-	
-    public function setOutputVector($outputsArray) 
+    }
+
+    public function setOutputVector($outputsArray)
     {
         $lastLayerNeurones = $this->getNeuronsByLayer($this->layers[count($this->layers) - 1]);
 
@@ -204,33 +217,33 @@ class Perceptron
             $lastLayerNeuron['cell']->setTargetOutput($outputsArray[$key++]);
             $this->updateNeuron($lastLayerNeuron['id'], $lastLayerNeuron);
         }
-	}
+    }
 
-    public function forwardPass() 
+    public function forwardPass()
     {
         foreach ($this->layers as $layer) {
-            
-			$neurones = $this->getNeuronsByLayer($layer);
+
+            $neurones = $this->getNeuronsByLayer($layer);
 
             foreach ($neurones as $neuron) {
-                
-				$inputSum = $neuron['cell']->getInput();
-				$leftLinks = $this->getNeuronLinks($neuron, 'left');
+
+                $inputSum = $neuron['cell']->getInput();
+                $leftLinks = $this->getNeuronLinks($neuron, 'left');
 
                 foreach ($leftLinks as $leftLink) {
                     $inputSum += $leftLink['neuron']['cell']->getOutput() * $leftLink['weight'];
-				}
+                }
 
-				$inputSum += $neuron['bias'];
+                $inputSum += $neuron['bias'];
 
-				$neuron['cell']->calcOutput($inputSum);
+                $neuron['cell']->calcOutput($inputSum);
 
-				$this->updateNeuron($neuron['id'], $neuron);
-			}
+                $this->updateNeuron($neuron['id'], $neuron);
+            }
         }
-	}
+    }
 
-    public function calcErrors() 
+    public function calcErrors()
     {
 
         $this->totalError = 0;
@@ -240,9 +253,9 @@ class Perceptron
             $neurones = $this->getNeuronsByLayer($this->layers[$li]);
 
             foreach ($neurones as $neuron) {
-				$rightLinks = $this->getNeuronLinks($neuron, 'right');
+                $rightLinks = $this->getNeuronLinks($neuron, 'right');
 
-				if (!$rightLinks) { // если это последний слой, то ошибка вычисляется разницей ожидания и выхода
+                if (!$rightLinks) { // если это последний слой, то ошибка вычисляется разницей ожидания и выхода
                     $neuron['cell']->setError($neuron['cell']->getTargetOutput() - $neuron['cell']->getOutput());
                 } else { // если слой скрытый, то ошибка - сумма ошибок правых узлов умноженных на веса.
                     $errorsSum = 0;
@@ -251,37 +264,42 @@ class Perceptron
                         $errorsSum += $rightLink['neuron']['cell']->getError() * $rightLink['weight'];
                     }
 
-					$neuron['cell']->setError($errorsSum);
-				}
+                    $neuron['cell']->setError($errorsSum);
+                }
 
-				$this->updateNeuron($neuron['id'], $neuron);
+                $this->updateNeuron($neuron['id'], $neuron);
 
-				if ($li === count($this->layers) - 1) {
+                if ($li === count($this->layers) - 1) {
                     $this->totalError += $neuron['cell']->getError();
                 }
             }
-		}
-	}
+        }
+    }
 
-    public function updateWeights() 
+    public function updateWeights()
     {
         foreach ($this->layers as $layer) {
 
-			$neurones = $this->getNeuronsByLayer($layer);
+            $neurones = $this->getNeuronsByLayer($layer);
 
             foreach ($neurones as $neuron) {
 
-				$rightLinks = $this->getNeuronLinks($neuron, 'right');
+                $rightLinks = $this->getNeuronLinks($neuron, 'right');
 
                 foreach ($rightLinks as $rightLink) {
                     $rightNeuron = $rightLink['neuron'];
-					$newWeight = $rightLink['weight'] + $rightNeuron['cell']->getDerivative() * $rightNeuron['cell']->getError() * $neuron['cell']->getOutput() * $this->getLearningRate();
-					$this->unlink($neuron['id'], $rightNeuron['id']);
-					$this->link($neuron['id'], $rightNeuron['id'], $newWeight);
-				}
-			}
+                    $newWeight =
+                        $rightLink['weight'] +
+                        $rightNeuron['cell']->getDerivative() *
+                        $rightNeuron['cell']->getError() *
+                        $neuron['cell']->getOutput() *
+                        $this->getLearningRate();
+                    $this->unlink($neuron['id'], $rightNeuron['id']);
+                    $this->link($neuron['id'], $rightNeuron['id'], $newWeight);
+                }
+            }
         }
-	}
+    }
 
     public function backPropagation()
     {
